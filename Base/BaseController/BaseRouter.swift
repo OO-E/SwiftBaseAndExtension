6c401ad6e1ae16008ee3ff94776b209b94b4f24a
Özgün Ergen <orhanozgun.ergen@kocsistem.com.tr>
2021-10-26 00:01:51 +0300

    first commit

---
+++ Base/BaseController/BaseRouter.swift
@@ -0,0 +1,17 @@
+//
+//  BaseRouter.swift
+//  WeatherApp
+//
+//  Created by OO E on 1.05.2021.
+//
+
+import UIKit
+
+
+class BaseRouter {
+    
+    var push = Observable<UIViewController>()
+    var present = Observable<UIViewController>()
+    
+    
+}
---
+++ Base/BaseController/BaseViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  BaseViewModel.swift
+//  WeatherApp
+//
+//  Created by OO E on 1.05.2021.
+//
+
+import Foundation
+
+
+class BaseViewModel {
+    
+    var errorState = Observable<ErrorState>()
+    var loaderState = Observable<LoaderState>()
+    
+}
+
+enum ErrorState {
+    
+    case showWith(title: String, message: String)
+    case networkError
+    case logout
+    case none
+}
+
+enum LoaderState {
+    case show
+    case dismiss
+}
